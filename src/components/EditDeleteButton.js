a46ef552664a36a90bc3441281f3744915c9c7e9
Phil-Welsh <pwelsh8@gmail.com>
2021-12-02 12:33:24 -0800

    Comment delete working

--- src/components/EditDeleteButton.js
+++ src/components/EditDeleteButton.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'
+import CommentModel from '../models/comment'
 
-const EditDeleteButton = () => {
-
-    const navigate = useNavigate()
+const EditDeleteButton = (props) => {
+    const navigate = useNavigate();
 
     const editButton = () => {
         navigate("./edit")
     }
 
+    const deleteButton = () => {
+        CommentModel.delete(props.commentId).then((data) => {
+        navigate(`/places/${props.placeId}`)
+        }
+    )
+}
+
     return (
         <div className="edit-delete-button">
             <button onClick={editButton}>
                 Edit
+            </button>
+            <button onClick={deleteButton}>
+                Delete
             </button>
         </div>
     );
